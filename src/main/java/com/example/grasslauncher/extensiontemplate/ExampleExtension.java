0953443df461caaf55ba8b821e6cef085269c7a7
Mr_limr267 <113236317+erha134@users.noreply.github.com>
2024-03-06 18:44:12 +0800

    更新 GrassLauncher 版本

--- src/main/java/com/example/grasslauncher/extensiontemplate/ExampleExtension.java
+++ src/main/java/com/example/grasslauncher/extensiontemplate/ExampleExtension.java
@@ -1,10 +1,14 @@
 package com.example.grasslauncher.extensiontemplate;
 
+import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
 import org.slf4j.Logger;
 import xyz.mrcraftteammc.grasslauncher.extension.Extension;
+import xyz.mrcraftteammc.grasslauncher.extension.ExtensionManifest;
 import xyz.mrcraftteammc.grasslauncher.extension.annotations.ExtensionInstance;
 import xyz.mrcraftteammc.grasslauncher.extension.exception.ExtensionException;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Random;
 
 @ExtensionInstance
@@ -12,13 +16,19 @@ public class ExampleExtension extends Extension {
     private final Logger logger = super.getLogger();
     private final Random random = new Random();
 
-    public ExampleExtension() {
-        super("example");
+    public ExampleExtension() throws IOException {
+        super(getExtensionManifest());
     }
 
     @Override
     public void onLoad() throws ExtensionException {
         logger.info("Hello ExampleExtension!");
         logger.info("Your lucky number today: {}", random.nextInt(100));
+    }
+
+    public static ExtensionManifest getExtensionManifest() throws IOException {
+        YAMLMapper mapper = new YAMLMapper();
+        InputStream stream = ExampleExtension.class.getResourceAsStream("/grasslauncher.extension.yml");
+        return mapper.readValue(stream, ExtensionManifest.class);
     }
 }
--- src/main/java/com/example/grasslauncher/extensiontemplate/FakeExampleExtension.java
+++ src/main/java/com/example/grasslauncher/extensiontemplate/FakeExampleExtension.java
@@ -1,11 +1,12 @@
 package com.example.grasslauncher.extensiontemplate;
 
 import xyz.mrcraftteammc.grasslauncher.extension.Extension;
+import xyz.mrcraftteammc.grasslauncher.extension.ExtensionManifest;
 import xyz.mrcraftteammc.grasslauncher.extension.exception.ExtensionException;
 
 public class FakeExampleExtension extends Extension {
     public FakeExampleExtension() {
-        super("example");
+        super(ExtensionManifest.defaultManifest());
     }
 
     @Override
